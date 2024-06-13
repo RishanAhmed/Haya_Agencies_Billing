d14b4ffe81b19f6859a7e25dd1c14db4ea389e1c
Rishan Ahmed <rishanhaya123@gmail.com>
2024-06-13 22:41:01 +0530

    New Updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,8 @@
 function Disply(val){
     const disobj = document.getElementById("MAIN"+val+"-ICON")
-    const Arrowv = document.getElementById("fa-chevron-down"+val)
+    const Arrowv = document.getElementById("bi-arrow-down"+val)
 
     Switch_Scroll_Arrow(Arrowv, disobj)
-
-    
     
     console.log(disobj.style)
 }
@@ -17,16 +15,16 @@ function Switch_Scroll_Arrow(shareva, disob){
 
     if(disobj.classList.contains("dis")){
         Direction = "Down"
-        shareval.classList.remove("bi-chevron-up")
-        shareval.classList.add("bi-chevron-right")
+        shareval.classList.remove("bi-arrow-up")
+        shareval.classList.add("bi-arrow-up-right")
     }
     else{
         Direction = "Up"
-        shareval.classList.remove("bi-chevron-down")
-        shareval.classList.add("bi-chevron-left")
+        shareval.classList.remove("bi-arrow-down")
+        shareval.classList.add("bi-arrow-down-left")
     }
 
-    setTimeout(second_Run, 180, shareval, Direction, disobj)
+    setTimeout(second_Run, 50, shareval, Direction, disobj)
 
     console.log('DONE')
 
@@ -34,16 +32,43 @@ function Switch_Scroll_Arrow(shareva, disob){
 
 function second_Run(shareval, dir, dis){
     if(dir == "Down"){
-        shareval.classList.remove("bi-chevron-right")
-        shareval.classList.add("bi-chevron-down")
+        shareval.classList.remove("bi-arrow-up-right")
+        shareval.classList.add("bi-arrow-right")
     }
     else{
-        shareval.classList.remove("bi-chevron-left")
-        shareval.classList.add("bi-chevron-up")
+        shareval.classList.remove("bi-arrow-down-left")
+        shareval.classList.add("bi-arrow-left")
+    }
+
+    setTimeout(Theird_Run, 50, shareval, dir, dis)
+}
+
+function Theird_Run(shareval, dir, dis){
+    if(dir == "Down"){
+        shareval.classList.remove("bi-arrow-right")
+        shareval.classList.add("bi-arrow-down-right")
+    }
+    else{
+        shareval.classList.remove("bi-arrow-left")
+        shareval.classList.add("bi-arrow-up-left")
+    }
+
+    setTimeout(Fourth_Run, 50, shareval, dir, dis)
+}
+
+function Fourth_Run(shareval, dir, dis){
+    if(dir == "Down"){
+        shareval.classList.remove("bi-arrow-down-right")
+        shareval.classList.add("bi-arrow-down")
+    }
+    else{
+        shareval.classList.remove("bi-arrow-up-left")
+        shareval.classList.add("bi-arrow-up")
     }
 
     DisplyVal(dir, dis)
 }
+
 
 function DisplyVal(Direc, disobj){
     if(Direc == "Down"){
